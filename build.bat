490c7203604d42216cf653220f1f7a2d963b1f7e
Ternt <kyle.pham202@gmail.com>
2026-04-13 09:26:09 -0400

    build + deployment actions files

--- build.bat
+++ build.bat
@@ -9,12 +9,10 @@ if not "%debug%"=="1" if not "%release%"=="1" set debug=1
 if "%debug%"=="1" set release=0 && echo [compile debug]
 if "%release%"=="1" set debug=0 && echo [compile release]
 
-if not "%msvc%"=="1" if not "%clang%"=="1" if not "%gcc%"=="1" if not "%wasm%"=="1" set msvc=1
-if "%gcc%"=="1"   set msvc=0 && set wasm=0 && set clang=0 && echo [compile gcc]
-if "%clang%"=="1" set msvc=0 && set wasm=0 && set gcc=0 && echo [compile clang]
-if "%msvc%"=="1"  set clang=0 && set wasm=0 && set gcc=0 && echo [compile msvc]
-if "%wasm%"=="1"  set clang=0 && set msvc=0 && set gcc=0 && echo [compile wasm]
-
+if not "%msvc%"=="1" if not "%clang%"=="1" if not "%wasm%"=="1" set msvc=1
+if "%clang%"=="1" set msvc=0 && set wasm=0 && echo [compile clang]
+if "%msvc%"=="1"  set clang=0 && set wasm=0 && echo [compile msvc]
+if "%wasm%"=="1"  set clang=0 && set msvc=0 && echo [compile wasm]
 if "%~1"==""                     echo [default mode, assuming `GAME` build] && set game=1
 if "%~1"=="release" if "%~2"=="" echo [default mode, assuming `GAME` build] && set game=1
 
@@ -39,11 +37,6 @@ set clang_debug=
 set clang_release= 
 set clang_link=    
 set clang_out=     
-set gcc_common=  
-set gcc_debug=   
-set gcc_release= 
-set gcc_link=    
-set gcc_out=     
 set wasm_common=    -sWASM=1
 set wasm_debug=     call emcc -O0 -g
 set wasm_release=   call emcc -O3
@@ -74,10 +67,6 @@ if "%clang%"=="1"   set compile_debug=%clang_debug%
 if "%clang%"=="1"   set compile_release=%clang_release%
 if "%clang%"=="1"   set compile_link=%clang_link%
 if "%clang%"=="1"   set out=%clang_out%
-if "%gcc%"=="1"     set compile_debug=%gcc_debug%
-if "%gcc%"=="1"     set compile_release=%gcc_release%
-if "%gcc%"=="1"     set compile_link=%gcc_link%
-if "%gcc%"=="1"     set out=%gcc_out%
 if "%wasm%"=="1"    set compile_debug=%wasm_debug%
 if "%wasm%"=="1"    set compile_release=%wasm_release%
 if "%wasm%"=="1"    set compile_link=%wasm_link%
@@ -88,23 +77,33 @@ if "%release%"=="1" set compile=%compile_release%
 :: -- Prepare Build Dir ----------------------------------------------------------------
 
 if not exist build mkdir build
-if not exist build/bin mkdir build/bin
+pushd build
+if not exist bin mkdir bin
+popd build
 
 :: -- Build ----------------------------------------------------------------------------
 
 pushd build
 pushd bin
-if "%game%"=="1"      set didbuild=1 && %compile% %root%code/game/game.c      %out%game.wasm || exit /b 1
-if "%test_wasm%"=="1" set didbuild=1 && %compile% %root%code/test/test_wasm.c %out%test_wasm.wasm || exit /b 1
+if "%game%"=="1"      if "%wasm%"=="1" set didbuild=1 && %compile% %root%code/game/game.c      %out%game.wasm || exit /b 1
+if "%game%"=="1"                       set didbuild=1 && %compile% %root%code/game/game.c      %out%game.wasm || exit /b 1
+if "%test_wasm%"=="1" if "%wasm%"=="1" set didbuild=1 && %compile% %root%code/test/test_wasm.c %out%test_wasm.wasm || exit /b 1
+if "%test_wasm%"=="1"                  set didbuild=1 && %compile% %root%code/test/test_wasm.c %out%test_wasm.wasm || exit /b 1
 popd bin
 popd build
 
 :: -- Post-Build Actions --------------------------------------------------------------
 
 if "%build_web%"=="1" (
-  if not exist build/web mkdir build/web
-  if not exist build/web/static mkdir build/web/static
-  call pnpm build >> NUL
+  pushd build
+    if not exist web mkdir web
+    pushd web
+      if not exist static mkdir static
+    popd web
+  popd build
+  cp code/web/*.js build/web/static/
+  cp code/web/*.css build/web/static/
+  cp code/web/*.html build/web/
   cp build/bin/*.wasm build/web/static/
 )
 
